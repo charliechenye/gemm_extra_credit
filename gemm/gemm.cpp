7a629d0f1976635a3d3c4779942e74773e67be92
Chenye Zhu <zhuchenye@gmail.com>
2021-11-29 00:10:20 -0800

    blocking implementation
    Update gemm.cpp

--- gemm/gemm.cpp
+++ gemm/gemm.cpp
@@ -10,19 +10,31 @@
 
 void gemm(int m, int n, int k, double *A, double *B, double *C, double alpha, double beta){
 
-    // REPLACE THIS WITH YOUR IMPLEMENTATION
+    // blocking implementation with block b_size x b_size
+    int b_size = 64;
+    // assumes:
+    // - n and k are multiples of b_size 
+    // - b_size * sizeof(int) is multiple of CACHE_LINE_SIZE
 
-    int i, j, kk;
-    for (i=0; i<m; i++){
-        for (j=0; j<n; j++){
-	    double inner_prod = 0;
-	    for (kk=0; kk<k; kk++){
-	        inner_prod += A[i*k+kk] * B[kk*n+j];
-	    }
-	    C[i*n+j] = alpha * inner_prod + beta * C[i*n+j];
-	}
-    }
-    
-    // END OF NAIVE IMPLEMENTATION
+    // A: m x n takes 1 x b_size
+    // B: n x k takes b_size x b_size
+    // C: m x k yields 1 x b_size
+    double sum;
+    int i, j, l, l_offset, j_offset;
+
+    for (i = 0; i < m; i ++)
+        for (j = 0; j < k; j ++)
+            C[i * m + j] = beta * C[i * m + j];
+
+
+    for (l_offset = 0; l_offset < n; l_offset += b_size)
+        for (j_offset = 0; j_offset < k; j_offset += b_size)
+            for (i = 0; i < m; i ++)
+                for (j = j_offset; j < j_offset + b_size; j ++) {
+                    sum = C[i * m + j];
+                    for (l = l_offset; l < l_offset + b_size; l ++)
+                        sum += A[i * m + l] * B[n * l + j] * alpha;
+                    C[i * m + j] = sum;
+                }   
 }
 
